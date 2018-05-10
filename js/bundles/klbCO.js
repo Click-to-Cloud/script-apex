f3af904f83185e400ea2a5ae62c3d93a274d9e3b
Miao (Wilson) Liu <wilson@clicktocloud.com>
2018-05-10 18:10:02 +1000

    add docs

---
+++ js/bundles/klbCO.js
@@ -0,0 +1,224 @@
+var pageComponent =
+webpackJsonppageComponent([10],{
+
+/***/ 268:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _metalComponent = __webpack_require__(1);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(2);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+__webpack_require__(6);
+
+__webpack_require__(7);
+
+__webpack_require__(8);
+
+__webpack_require__(9);
+
+__webpack_require__(10);
+
+__webpack_require__(11);
+
+__webpack_require__(12);
+
+__webpack_require__(13);
+
+__webpack_require__(14);
+
+__webpack_require__(15);
+
+__webpack_require__(16);
+
+__webpack_require__(17);
+
+var _indexSoy = __webpack_require__(269);
+
+var _indexSoy2 = _interopRequireDefault(_indexSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var klbCO = function (_Component) {
+  _inherits(klbCO, _Component);
+
+  function klbCO() {
+    _classCallCheck(this, klbCO);
+
+    return _possibleConstructorReturn(this, (klbCO.__proto__ || Object.getPrototypeOf(klbCO)).apply(this, arguments));
+  }
+
+  return klbCO;
+}(_metalComponent2.default);
+
+;
+
+_metalSoy2.default.register(klbCO, _indexSoy2.default);
+
+exports.default = klbCO;
+
+/***/ }),
+
+/***/ 269:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.templates = exports.klbCO = undefined;
+
+var _metalComponent = __webpack_require__(1);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(2);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* jshint ignore:start */
+
+
+var templates;
+goog.loadModule(function (exports) {
+
+  // This file was automatically generated from index.soy.
+  // Please don't edit this file by hand.
+
+  /**
+   * @fileoverview Templates in namespace klbCO.
+   * @public
+   */
+
+  goog.module('klbCO.incrementaldom');
+
+  /** @suppress {extraRequire} */
+  var soy = goog.require('soy');
+  /** @suppress {extraRequire} */
+  var soydata = goog.require('soydata');
+  /** @suppress {extraRequire} */
+  goog.require('goog.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('soy.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('goog.i18n.bidi');
+  /** @suppress {extraRequire} */
+  goog.require('goog.string');
+  var IncrementalDom = goog.require('incrementaldom');
+  var ie_open = IncrementalDom.elementOpen;
+  var ie_close = IncrementalDom.elementClose;
+  var ie_void = IncrementalDom.elementVoid;
+  var ie_open_start = IncrementalDom.elementOpenStart;
+  var ie_open_end = IncrementalDom.elementOpenEnd;
+  var itext = IncrementalDom.text;
+  var iattr = IncrementalDom.attr;
+
+  var $templateAlias2 = _metalSoy2.default.getTemplate('ElectricCode.incrementaldom', 'render');
+
+  var $templateAlias1 = _metalSoy2.default.getTemplate('guide.incrementaldom', 'render');
+
+  /**
+   * @param {{
+   *    page: (?),
+   *    site: (?)
+   * }} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $render(opt_data, opt_ignored, opt_ijData) {
+    var param315 = function param315() {
+      ie_open('h6');
+      var dyn27 = opt_data.page.description;
+      if (typeof dyn27 == 'function') dyn27();else if (dyn27 != null) itext(dyn27);
+      ie_close('h6');
+      ie_open('article', null, null, 'id', '1');
+      ie_open('h2');
+      itext('Simple Evaluation');
+      ie_close('h2');
+      ie_open('p');
+      itext('Simple expressions can be passed directly to evaluate them.');
+      ie_close('p');
+      $templateAlias2({ code: 'Object result = ScriptEngine.getInstance().eval(\'1 + 2 * (3 - 1)\');', mode: 'javascript' }, null, opt_ijData);
+      ie_close('article');
+      ie_open('article', null, null, 'id', '2');
+      ie_open('h2');
+      itext('Context Evaluation');
+      ie_close('h2');
+      ie_open('p');
+      itext('We can also pass in a context with variables to evaluate the expression.');
+      ie_close('p');
+      $templateAlias2({ code: 'Map<String, Object> context = new Map<String, Object>{\n    \'a\' => 1,\n    \'b\' => 2\n};\nObject result = ScriptEngine.getInstance().eval(\'a + b\', context);', mode: 'javascript' }, null, opt_ijData);
+      ie_close('article');
+      ie_open('article', null, null, 'id', '3');
+      ie_open('h2');
+      itext('Limitations');
+      ie_close('h2');
+      ie_open('p');
+      itext('Assignment expressions and function invocations are not supported in evaluation of JavaScript expressions.');
+      ie_close('p');
+      ie_close('article');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.page.title);
+      ie_close('input');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.site.title);
+      ie_close('input');
+    };
+    $templateAlias1(soy.$$assignDefaults({ content: param315 }, opt_data), null, opt_ijData);
+  }
+  exports.render = $render;
+  if (goog.DEBUG) {
+    $render.soyTemplateName = 'klbCO.render';
+  }
+
+  exports.render.params = ["page", "site"];
+  exports.render.types = { "page": "?", "site": "?" };
+  exports.templates = templates = exports;
+  return exports;
+});
+
+var klbCO = function (_Component) {
+  _inherits(klbCO, _Component);
+
+  function klbCO() {
+    _classCallCheck(this, klbCO);
+
+    return _possibleConstructorReturn(this, (klbCO.__proto__ || Object.getPrototypeOf(klbCO)).apply(this, arguments));
+  }
+
+  return klbCO;
+}(_metalComponent2.default);
+
+_metalSoy2.default.register(klbCO, templates);
+exports.klbCO = klbCO;
+exports.templates = templates;
+exports.default = templates;
+/* jshint ignore:end */
+
+/***/ })
+
+},[268]);
---
+++ js/bundles/vEqlT.js
@@ -0,0 +1,464 @@
+var pageComponent =
+webpackJsonppageComponent([9],{
+
+/***/ 270:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _metalComponent = __webpack_require__(1);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(2);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+__webpack_require__(6);
+
+__webpack_require__(7);
+
+__webpack_require__(8);
+
+__webpack_require__(9);
+
+__webpack_require__(10);
+
+__webpack_require__(11);
+
+__webpack_require__(12);
+
+__webpack_require__(13);
+
+__webpack_require__(14);
+
+__webpack_require__(15);
+
+__webpack_require__(16);
+
+__webpack_require__(17);
+
+var _indexSoy = __webpack_require__(271);
+
+var _indexSoy2 = _interopRequireDefault(_indexSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var vEqlT = function (_Component) {
+  _inherits(vEqlT, _Component);
+
+  function vEqlT() {
+    _classCallCheck(this, vEqlT);
+
+    return _possibleConstructorReturn(this, (vEqlT.__proto__ || Object.getPrototypeOf(vEqlT)).apply(this, arguments));
+  }
+
+  return vEqlT;
+}(_metalComponent2.default);
+
+;
+
+_metalSoy2.default.register(vEqlT, _indexSoy2.default);
+
+exports.default = vEqlT;
+
+/***/ }),
+
+/***/ 271:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.templates = exports.vEqlT = undefined;
+
+var _metalComponent = __webpack_require__(1);
+
+var _metalComponent2 = _interopRequireDefault(_metalComponent);
+
+var _metalSoy = __webpack_require__(2);
+
+var _metalSoy2 = _interopRequireDefault(_metalSoy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /* jshint ignore:start */
+
+
+var templates;
+goog.loadModule(function (exports) {
+
+  // This file was automatically generated from index.soy.
+  // Please don't edit this file by hand.
+
+  /**
+   * @fileoverview Templates in namespace vEqlT.
+   * @public
+   */
+
+  goog.module('vEqlT.incrementaldom');
+
+  /** @suppress {extraRequire} */
+  var soy = goog.require('soy');
+  /** @suppress {extraRequire} */
+  var soydata = goog.require('soydata');
+  /** @suppress {extraRequire} */
+  goog.require('goog.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('soy.asserts');
+  /** @suppress {extraRequire} */
+  goog.require('goog.i18n.bidi');
+  /** @suppress {extraRequire} */
+  goog.require('goog.string');
+  var IncrementalDom = goog.require('incrementaldom');
+  var ie_open = IncrementalDom.elementOpen;
+  var ie_close = IncrementalDom.elementClose;
+  var ie_void = IncrementalDom.elementVoid;
+  var ie_open_start = IncrementalDom.elementOpenStart;
+  var ie_open_end = IncrementalDom.elementOpenEnd;
+  var itext = IncrementalDom.text;
+  var iattr = IncrementalDom.attr;
+
+  var $templateAlias2 = _metalSoy2.default.getTemplate('ElectricCode.incrementaldom', 'render');
+
+  var $templateAlias1 = _metalSoy2.default.getTemplate('guide.incrementaldom', 'render');
+
+  /**
+   * @param {{
+   *    page: (?),
+   *    site: (?)
+   * }} opt_data
+   * @param {(null|undefined)=} opt_ignored
+   * @param {Object<string, *>=} opt_ijData
+   * @return {void}
+   * @suppress {checkTypes}
+   */
+  function $render(opt_data, opt_ignored, opt_ijData) {
+    var param336 = function param336() {
+      ie_open('h6');
+      var dyn28 = opt_data.page.description;
+      if (typeof dyn28 == 'function') dyn28();else if (dyn28 != null) itext(dyn28);
+      ie_close('h6');
+      ie_open('article', null, null, 'id', '1');
+      ie_open('h2');
+      itext('Parsing Expression');
+      ie_close('h2');
+      ie_open('p');
+      itext('Here is how we can parse JavaScript expressions.');
+      ie_close('p');
+      $templateAlias2({ code: 'Jsep.Node node = new Jsep(\'1 + 2\').parse();', mode: 'javascript' }, null, opt_ijData);
+      ie_close('article');
+      ie_open('article', null, null, 'id', '2');
+      ie_open('h2');
+      itext('Expression Support');
+      ie_close('h2');
+      ie_open('p');
+      itext('Supported Unary Operator');
+      ie_close('p');
+      ie_open('table');
+      ie_open('thead');
+      ie_open('tr');
+      ie_open('th');
+      itext('Name');
+      ie_close('th');
+      ie_open('th');
+      itext('Description');
+      ie_close('th');
+      ie_close('tr');
+      ie_close('thead');
+      ie_open('tbody');
+      ie_open('tr');
+      ie_open('td');
+      itext('-');
+      ie_close('td');
+      ie_open('td');
+      itext('Negate number');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('!');
+      ie_close('td');
+      ie_open('td');
+      itext('Negate boolean');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('++');
+      ie_close('td');
+      ie_open('td');
+      itext('Only prefix supported');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('--');
+      ie_close('td');
+      ie_open('td');
+      itext('Only prefix supported');
+      ie_close('td');
+      ie_close('tr');
+      ie_close('tbody');
+      ie_close('table');
+      ie_open('p');
+      itext('Supported Logical Operator');
+      ie_close('p');
+      ie_open('table');
+      ie_open('thead');
+      ie_open('tr');
+      ie_open('th');
+      itext('Name');
+      ie_close('th');
+      ie_open('th');
+      itext('Description');
+      ie_close('th');
+      ie_close('tr');
+      ie_close('thead');
+      ie_open('tbody');
+      ie_open('tr');
+      ie_open('td');
+      itext('&&');
+      ie_close('td');
+      ie_open('td');
+      itext('Logical and');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('\\');
+      ie_close('td');
+      ie_open('td');
+      itext('\\');
+      ie_close('td');
+      ie_void('td');
+      ie_open('td');
+      itext('Logical or');
+      ie_close('td');
+      ie_close('tr');
+      ie_close('tbody');
+      ie_close('table');
+      ie_open('p');
+      itext('Supported Binary Operator');
+      ie_close('p');
+      ie_open('table');
+      ie_open('thead');
+      ie_open('tr');
+      ie_open('th');
+      itext('Name');
+      ie_close('th');
+      ie_open('th');
+      itext('Description');
+      ie_close('th');
+      ie_close('tr');
+      ie_close('thead');
+      ie_open('tbody');
+      ie_open('tr');
+      ie_open('td');
+      itext('==');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex ==');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('!=');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex !=');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('===');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex ==');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('!==');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex !=');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('<');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex <');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('>');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex >');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('<=');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex <=');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('>=');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex >=');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('+');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex +');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('-');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex -');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('*');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex *');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('/');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex /');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('%');
+      ie_close('td');
+      ie_open('td');
+      itext('Apex Math.mod');
+      ie_close('td');
+      ie_close('tr');
+      ie_close('tbody');
+      ie_close('table');
+      ie_open('p');
+      itext('Supported Structure');
+      ie_close('p');
+      ie_open('table');
+      ie_open('thead');
+      ie_open('tr');
+      ie_open('th');
+      itext('Name');
+      ie_close('th');
+      ie_open('th');
+      itext('Description');
+      ie_close('th');
+      ie_close('tr');
+      ie_close('thead');
+      ie_open('tbody');
+      ie_open('tr');
+      ie_open('td');
+      itext('Conditional Expression(? :)');
+      ie_close('td');
+      ie_open('td');
+      itext('Yes');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('Array Literal');
+      ie_close('td');
+      ie_open('td');
+      itext('Yes');
+      ie_close('td');
+      ie_close('tr');
+      ie_open('tr');
+      ie_open('td');
+      itext('Object Literal');
+      ie_close('td');
+      ie_open('td');
+      itext('Yes');
+      ie_close('td');
+      ie_close('tr');
+      ie_close('tbody');
+      ie_close('table');
+      ie_open('p');
+      itext('For example,');
+      ie_close('p');
+      $templateAlias2({ code: 'ScriptEngine.getInstance.eval(\'["a", "b"]\'); // Array literal\n\nScriptEngine.getInstance.eval(\'{ "name": "test", age: 18 }\'); // Object literal', mode: 'java' }, null, opt_ijData);
+      ie_close('article');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.page.title);
+      ie_close('input');
+      ie_open('input', null, null, 'type', 'hidden', 'value', opt_data.site.title);
+      ie_close('input');
+    };
+    $templateAlias1(soy.$$assignDefaults({ content: param336 }, opt_data), null, opt_ijData);
+  }
+  exports.render = $render;
+  if (goog.DEBUG) {
+    $render.soyTemplateName = 'vEqlT.render';
+  }
+
+  exports.render.params = ["page", "site"];
+  exports.render.types = { "page": "?", "site": "?" };
+  exports.templates = templates = exports;
+  return exports;
+});
+
+var vEqlT = function (_Component) {
+  _inherits(vEqlT, _Component);
+
+  function vEqlT() {
+    _classCallCheck(this, vEqlT);
+
+    return _possibleConstructorReturn(this, (vEqlT.__proto__ || Object.getPrototypeOf(vEqlT)).apply(this, arguments));
+  }
+
+  return vEqlT;
+}(_metalComponent2.default);
+
+_metalSoy2.default.register(vEqlT, templates);
+exports.vEqlT = vEqlT;
+exports.templates = templates;
+exports.default = templates;
+/* jshint ignore:end */
+
+/***/ })
+
+},[270]);
